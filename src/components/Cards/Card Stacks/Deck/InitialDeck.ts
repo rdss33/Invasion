625904339d1ffe2bd0561f0925511783d47cf059
rdss33 <raphaeldssoares@gmail.com>
2025-06-15 15:16:50 -0300

    Erro Cleanup
    Cleaning some errors

--- src/components/Cards/Card Stacks/Deck/InitialDeck.ts
+++ src/components/Cards/Card Stacks/Deck/InitialDeck.ts
@@ -1,11 +1,11 @@
-import { Guard, GuardInformation, GuardAttributes } from "../../Battle Cards/Guard"
+import { Guard} from "../../Battle Cards/Guard"
 import { Card } from "../../Card"
 
 export const INITIAL_DECK : Card[] = [
-    new Guard(GuardInformation, GuardAttributes),
-    new Guard(GuardInformation, GuardAttributes),
-    new Guard(GuardInformation, GuardAttributes),
-    new Guard(GuardInformation, GuardAttributes),
-    new Guard(GuardInformation, GuardAttributes),
-    new Guard(GuardInformation, GuardAttributes),
+    new Guard(),
+    new Guard(),
+    new Guard(),
+    new Guard(),
+    new Guard(),
+    new Guard(),
 ]
